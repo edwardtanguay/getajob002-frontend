5aecba9218329ce19c909923361270e0fd81b757
Edward Tanguay (windows) <edward@tanguay.info>
2022-12-06 10:29:36 +0100

    frontend finished with json data

--- src/pages/PageJobs.tsx
+++ src/pages/PageJobs.tsx
@@ -1,7 +1,38 @@
+import _jobs from '../data/jobs.json';
+
+type Job = {
+    id: number;
+    title: string;
+    company: string;
+    description: string;
+    url: string;
+    skillList: string;
+    todo: string;
+};
+ 
+const jobs: Job[] = _jobs as any[];
+
 export const PageJobs = () => {
 	return (
 		<div className="page pageJobs">
-			<p>This is the Jobs page.</p>
+			<div className="jobs">
+				<h2>There are {jobs.length} jobs:</h2>
+				{jobs.map((job) => {
+					return (
+						<div className="job" key={job.id}>
+							<div className="title">
+								<a href={job.url} target="_blank">
+									{job.title}
+								</a>
+							</div>
+							<div className="company">{job.company}</div>
+							<div className="description">{job.description}</div>
+							<div className="skills">{job.skillList}</div>
+							<div className="todo">{job.todo}</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
